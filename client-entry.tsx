dc0352b5db63a2c8566c05c9bcc95ea1128c47bf
yoshinori yokoyama <nominalrune.yokoyama@gmail.com>
2024-08-07 21:57:45 +0900

    mmmm

--- client-entry.tsx
+++ client-entry.tsx
@@ -2,8 +2,8 @@ import config from './package.json';
 import { Summary } from './src/components/Summary';
 import { remarkSummaryPlugin } from './src/sevices/summary';
 import { Options, Func, ViewOptions } from './types/utils';
-
-declare const growiFacade : {
+import Logger from 'bunyan';
+declare const growiFacade: {
   markdownRenderer?: {
     optionsGenerators: {
       customGenerateViewOptions: (path: string, options: Options, toc: Func) => ViewOptions,
@@ -13,21 +13,27 @@ declare const growiFacade : {
 };
 
 const activate = (): void => {
+  // logger.debug()
+  const logger = Logger.createLogger({
+    name: 'growi'
+  });
+  throw new Error('plugin-summarize activation!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+
   if (growiFacade == null || growiFacade.markdownRenderer == null) {
     return;
   }
 
-  const { optionsGenerators } = growiFacade.markdownRenderer;
+  // const { optionsGenerators } = growiFacade.markdownRenderer;
 
-  optionsGenerators.customGenerateViewOptions = (...args) => {
-    const options = optionsGenerators.generateViewOptions(...args);
-    //@ts-expect-error
-    options.components.summary = Summary(options.components.summary); // Wrap the default component
-    //@ts-expect-error
-    options.remarkPlugins.push([remarkSummaryPlugin]);
-    console.log(options);
-    return options;
-  };
+  // optionsGenerators.customGenerateViewOptions = (...args) => {
+  //   const options = optionsGenerators.generateViewOptions(...args);
+  //   //@ts-expect-error
+  //   options.components.summary = Summary(options.components.summary); // Wrap the default component
+  //   //@ts-expect-error
+  //   options.remarkPlugins.push(remarkSummaryPlugin);
+  //   console.info(options);
+  //   return options;
+  // };
 };
 
 const deactivate = (): void => {
